b7f8740f813deda17d0971ea08c1d6c5f295fcf6
Hodhod <alameddinehodhod@hotmail.com>
2021-12-11 22:54:37 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,10 @@
+Basic Queries
+Answer :
+1 : SELECT name FROME students 
+2 : SELECT * FROME students where age >30
+3 : SELECT name FROM students WHERE age = 30 and Gender = "F"
+4 : SELECT Points FROM students WHERE name="Alex"
+5 : INSERT INTO students (name, age, gender, points) VALUES ("Hodhod", "26", "M", "200")
+6 : UPDATE students SET Points = 400 where name = "Basma"
+7 : UPDATE students SET Points = 150 where name = "Alex"
+
