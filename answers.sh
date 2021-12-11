cabfe6637ee2f770a011dffd156aa817e713df8e
Hodhod <alameddinehodhod@hotmail.com>
2021-12-11 23:39:44 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -7,4 +7,13 @@ Answer :
 5 : INSERT INTO students (name, age, gender, points) VALUES ("Hodhod", "26", "M", "200")
 6 : UPDATE students SET Points = 400 where name = "Basma"
 7 : UPDATE students SET Points = 150 where name = "Alex"
+8 : Remember to update **answers.sh** file
+9 : commit 
 
+Execute SQL
+
+1 : INSERT INTO graduates ("Name", "Age", "Gender", "Points") 
+SELECT name, Age, Gender, Points FROM students WHERE id = 4
+2 : UPDATE graduates set Graduation = 08/09/2018 WHERE name = "Layla"
+3 : DELETE FROM students WHERE id = 4
+4 : Commit \("Creating Table"\)
